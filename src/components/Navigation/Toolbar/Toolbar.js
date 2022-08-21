a9d424aa4c0279ff7221e8b7e20b941529de7a14
Subhajit Kundu <jitkundu6@gmail.com>
2022-08-22 03:18:48 +0530

    Burger Builder: Added Logo in the Toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ToolbarCSS from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
 
 const toolbar = () => (
     <header className={ToolbarCSS.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ...
         </nav>
