7ea29eb1b07ab233a7d39cb0997eef55847c3862
Subhajit Kundu <jitkundu6@gmail.com>
2022-08-23 04:24:08 +0530

    Burger Builder: Modified Toggle MENU Button

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+import DrawerToggleCSS from "./DrawerToggle.module.css";
+
 const drawerToggle = (props) => (
-    <div onClick={props.clicked}>
-        MENU
+    <div className={DrawerToggleCSS.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 );
 
