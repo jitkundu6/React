894c502d15aa1b3486b491e939928001be55675c
Subhajit Kundu <jitkundu6@gmail.com>
2022-08-31 05:37:11 +0530

    Image Gallery App: Filter images based on location

--- src/App.js
+++ src/App.js
@@ -1,80 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Person from './Person/Person';
+import Form from './components/Form/Form';
+import ImageList from './imageList.json';
 
 class App extends Component {
   state = {
-    person: [
-      {name: "Micky", age: 5},
-      {name: "Mini", age: 4},
-      {name: "Donal", age: 6},      
-    ],
-    otherState: "This is some other state.",
+    country: 'all',
+    state: 'all',
+    city: 'all',
   };
 
-
-  switchNameHandler = (newName) => {
-    console.log("Switch name clicked.");
+  resetHandler = () => {
     this.setState({
-        person: [
-          {name: newName, age: 7},
-          {name: "Mini", age: 4},
-          {name: "Donal", age: 9},      
-        ],
+      country: 'all',
+      state: 'all',
+      city: 'all',
+    });
+  }
+
+  countryHandler = (event) => {
+    console.log("Country changed!");
+    console.log(this.state);
+    this.setState({
+        country: event.target.value,
+        state: 'all',
+        city: 'all',
+    });
+  }
+
+  stateHandler = (event) => {
+      console.log("State changed!");
+      console.log(this.state);
+      this.setState({
+          state: event.target.value,
+          city: 'all',
       });
   }
 
-  nameChangedHandler = (event) => {
-    console.log("Switch name clicked.");
-    this.setState({
-        person: [
-          {name: event.target.value, age: 7},
-          {name: "Mini", age: 4},
-          {name: "Donal", age: 9},      
-        ],
+  cityHandler = (event) => {
+      console.log("City changed!");
+      console.log(this.state);
+      this.setState({
+          city: event.target.value,
       });
   }
+
 
   render() {
     console.log(this.state);
 
-    const btStyle = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '10px',
-      cursor: 'pointer',
-    };
+    let images = ImageList.map((img) => {
+      if (img && (this.state.country==="all" ||  this.state.country===img.country))
+        return (img);
+    })
+    .map((img) => {
+      if(img && (this.state.state==="all" ||  this.state.state===img.state))
+        return (img);
+    })
+    .map((img) => {
+      if(img && (this.state.city==="all" ||  this.state.city===img.city))
+        return (img);
+    })
+    .map((img, index) => {
+      if(img)
+        return (
+            <img key={index} src={img.image} alt="" className='Image'/>
+        );
+    });
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <h1> My 3rd React app with User Input / Output + css :-) </h1>
-        <button 
-          onClick={() => this.switchNameHandler("Micky Mouse!")}
-          style={btStyle}>
-            Switch-Name
-        </button>
-        <Person 
-          name={this.state.person[0].name}
-          age={this.state.person[0].age}
-          changed={this.nameChangedHandler}
-          />
-        <Person 
-          name={this.state.person[1].name}
-          age={this.state.person[1].age}
-          click={this.switchNameHandler.bind(this, 'Funny Micky')}>
-            <button> I will make Micky funny! </button></Person>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age} />
+        <Form 
+          state={this.state}
+          onSubmit={this.resetHandler} 
+          countryHandler={this.countryHandler}
+          stateHandler={this.stateHandler}
+          cityHandler={this.cityHandler}
+        />
+        {images}
       </div>
     );
   }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,78 @@
+import React, { Component} from "react";
+import './Form.css';
+import ImageList from '../../imageList.json';
+
+class Form extends Component {
+
+    removeDuplicates(arr) {
+        return [...new Set(arr)];
+    }
+
+    render() {
+        let imageCountries = ImageList.map((img) => (
+            img.country
+        ));
+        let countries = this.removeDuplicates(imageCountries).map((country, index) => (<option key={index}> {country} </option>));
+
+
+        let imageStates = ImageList.map((img) => {
+            if(this.props.state.country && (this.props.state.country==="all" ||  this.props.state.country===img.country))
+                return(img.state);
+        });
+        let states = this.removeDuplicates(imageStates).map((state, index) => {
+            if (state)
+                return(<option key={index}> {state} </option>)
+        });
+
+
+        let imageCities = ImageList
+        .map((img) => {
+            if(this.props.state.country && (this.props.state.country==="all" ||  this.props.state.country===img.country))
+                return(img);
+        })
+        .map((img) => {
+            if(img && this.props.state.state && (this.props.state.state==="all" ||  this.props.state.state===img.state))
+                return(img.city);
+        });
+        let cities = this.removeDuplicates(imageCities).map((city, index) => {
+            if (city)
+                return(<option key={index}> {city} </option>)
+        });
+
+        return(
+            <div className="Form">
+                <form onSubmit={() => this.props.onSubmit()}>     
+                    <header className="Form_header">
+                        <h2> Image Gallery </h2>
+                    </header>
+                    <h3>
+                    <label> Country </label>
+                    <select nmae="country" onChange={this.props.countryHandler} value={this.props.state.country}>
+                        <option>all</option>
+                        {countries}
+                    </select>
+
+                    <label> State </label>
+                    <select nmae="country" onChange={this.props.stateHandler} value={this.props.state.state}>
+                        <option>all</option>
+                        {states}
+                    </select>
+
+                    <label> City </label>
+                    <select nmae="country" onChange={this.props.cityHandler} value={this.props.state.city}>
+                        <option>all</option>
+                        {cities}
+                    </select>
+
+                    <div>
+                        <button className="submit_button" type="submit"> RESET </button>
+                    </div>
+                    </h3>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Form;                
+               
